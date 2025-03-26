c8a38f0326ca37365618389450194523b5277a18
ANGELACAMU <anyo333@gmail.com>
2025-03-26 13:25:07 +0100

    método for loops

---
+++ Fundamentos JS/Js/metodo-for-loops.js
@@ -0,0 +1,18 @@
+//LOOP : FOR
+//nos va a ayudar a iterar con elejemtos que tengan ciertos objetos iterables y a pasarlos uno por uno
+
+//estructura for
+
+//for (variable; condición; incremento) {
+//    código a ejecutar
+//}
+
+let list = ["eat", "sleep", "code", "repeat"];
+
+//mientras 0 sea menor que la longitud de 4 de nuestra lista incrementa x1, mientras la condicion se cumpla se incrementa
+for (let i = 0; i < list.length; i++) {
+    console.log(list[i])
+}
+
+
+
