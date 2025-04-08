00d588e277eba6e861b887a4336c546aa6be2732
ANGELACAMU <anyo333@gmail.com>
2025-04-08 14:55:59 +0200

    prototype and inheritance

---
+++ Fundamentos JS/Js/39-prototipos-herencias.js
@@ -0,0 +1,10 @@
+/*
+Cuando hablamos de JavaScript y de programaciones a objetos hay dos temas importantes:
+
+1-JavaScript funciona con objetos y prototipos
+
+2-Programacion orientada a objetos funciona con herencia
+
+
+Una vez que generamos el método que extiende de la clase padre (la clase principal que tenemos) gracias al prototipo que se genero de la clase constructora, podemos heredar los métodos que fueron creados de esta clase padre.
+*/
