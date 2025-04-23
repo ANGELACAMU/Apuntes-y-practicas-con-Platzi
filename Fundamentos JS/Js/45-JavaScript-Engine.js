47278408f73eafb035668394aee33a956fd79607
ANGELACAMU <anyo333@gmail.com>
2025-04-23 12:34:48 +0200

    Js Engine Notes

---
+++ Fundamentos JS/Js/45-JavaScript-Engine.js
@@ -0,0 +1,19 @@
+/*
+¿COMO FUNCIONA JS ENGINE?  Para entender funciones sincronas y asincronas
+
+MEMORY HEAP- 
+Espacio donde se guardan variables, funciones, objetos...
+
+
+CALL STACK-
+Es la pila donde se llaman las funciones que tenemos que ejecutar
+
+Funcion síncrona- Una tarea a la vez
+
+Stack over flow- Cuando llevamos nuestras tareas al extremo de exceder el numero o memoria que tiene permitida para ejecutarlas, el ordenador crashea
+
+Función asincrona- en simultáneo
+
+WEB API- gracias a esto podemos hacer que nuestro codigo pueda comportarse como código asíncrono, podemos usar para ello "promesas" que tambien pueden ser sync and await que nos permiten que nuestro código pueda ser de asíncrono y se ejecute en paralelo
+
+*/
