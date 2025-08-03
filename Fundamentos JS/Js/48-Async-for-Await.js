caa43eb77e299b3ba4f3ab0826f1a89e3aad46a7
ANGELACAMU <anyo333@gmail.com>
2025-08-03 21:11:36 +0200

    Async promises Await

---
+++ Fundamentos JS/Js/48-Async-for-Await.js
@@ -0,0 +1,33 @@
+//Partiendo del ejercicio anterior
+//Hacer mas de una petición con: for await off
+async function fetchData() {
+    try {
+        let response = await fetch("https://rickandmortyapi.com/api/character")
+        let data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+
+    }
+
+}
+
+const urls =  ["https://rickandmortyapi.com/api/character ",
+     "https://rickandmortyapi.com/api/location",
+    "https://rickandmortyapi.com/api/episode ",
+];
+
+async function fetchNewData() {
+    try {
+        for await (let url of urls) {
+            let response = await fetch(url);
+            let data = await response.json
+            ();
+            console.log(data)
+        }
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+fetchNewData();
